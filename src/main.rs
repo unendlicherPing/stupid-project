d754b5c133272173ade7562f0bb9c8c58da226ed
Max Mustermann <max@musterman.de>
2022-11-23 17:11:27 +0100

    feat(parser): Parse arguments
    Now parses the first argument in the StupidArgs struct and returns an
    error if there are to much arguments.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,38 @@
 //! This will be a very stupid project.
 
+use std::env::Args;
+
+enum StupidError {
+    ToMuchArgs
+}
+
+type StupidResult<T> = Result<T, StupidError>;
+
+struct StupidArgs {
+    msg: String,
+}
+
+impl StupidArgs {
+    fn parse_args(args: Args) -> StupidResult<Self> {
+        let args: Vec<String> = args.collect();
+        
+        if args.len() >= 1 {
+            return Err(StupidError::ToMuchArgs)
+        }
+
+        let arg = args.get(0).unwrap();
+
+        Ok(Self { msg: arg.to_owned() })
+    }
+
+    fn msg(&self) -> &String { &self.msg }
+}
 
 fn main() {
-    println!("Hello, world!");
+    let args: StupidArgs = match StupidArgs::parse_args(std::env::args()) {
+        Ok(args) => args,
+        Err(_) => panic!("To much arguments btw."),
+    };
+
+    println!("Your message was: {}", args.msg())
 }
